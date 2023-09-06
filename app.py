d458457dc5b7670b4598551fef268328707d1ee5
anasshabrah <69520150+anasshabrah@users.noreply.github.com>
2023-09-07 01:27:44 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import model  # This will be your machine learning script.
+import logging
 
 app = Flask(__name__)
+
+# Set logging level after the app instance is created.
+app.logger.setLevel(logging.DEBUG)
 
 @app.route("/predict", methods=["POST"])
 def predict():
