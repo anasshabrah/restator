399d261b64d06debda1a4c4b5d29b105de07e543
anasshabrah <69520150+anasshabrah@users.noreply.github.com>
2023-09-07 01:37:50 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,16 +4,29 @@ import logging
 
 app = Flask(__name__)
 
+# Set up logging configuration
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+
 # Set logging level after the app instance is created.
 app.logger.setLevel(logging.DEBUG)
 
+# Add stream handler to send logs to STDOUT.
+stream_handler = logging.StreamHandler()
+stream_handler.setLevel(logging.DEBUG)
+app.logger.addHandler(stream_handler)
+
 @app.route("/predict", methods=["POST"])
 def predict():
-    # Here you will handle the prediction.
-    # This requires refactoring your model.py script.
-    data = request.get_json(force=True)
-    prediction = model.predict(data)
-    return jsonify({"prediction": prediction})
+    try:
+        # Here you will handle the prediction.
+        # This requires refactoring your model.py script.
+        data = request.get_json(force=True)
+        prediction = model.predict(data)
+        app.logger.info('Prediction successfully made.')
+        return jsonify({"prediction": prediction})
+    except Exception as e:
+        app.logger.error(f"Error during prediction: {e}")
+        return jsonify({"error": "Error during prediction", "details": str(e)}), 500
 
 if __name__ == "__main__":
     app.run(debug=True)
